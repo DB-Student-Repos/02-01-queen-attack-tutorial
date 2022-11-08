9e627cda96077e7678e683b8d2cf6afab6b58db7
shreeja-shreeja <115733593+shreeja-shreeja@users.noreply.github.com>
2022-11-08 22:46:05 +0100

    Update queen_attack.py

--- queen_attack.py
+++ queen_attack.py
@@ -1,6 +1,23 @@
 class Queen:
     def __init__(self, row, column):
-        pass
+        self.row = row
+        self.column = column
+        if (row < 0):
+            raise ValueError ("row not positive")
+        if (row > 7):
+            raise ValueError ("row not on board")
+        if (column < 0):
+            raise ValueError ("column not positive")
+        if (column > 7):
+            raise ValueError ("column not on board")
+        return
 
     def can_attack(self, another_queen):
-        pass
+        if ((self.row - another_queen.row) == 0):
+            return True
+        elif ((self.column - another_queen.column) == 0):
+            return True
+        elif ((self.row - another_queen.row) == (self.column - another_queen.column)) or ((self.row - another_queen.row) == (another_queen.column - self.column)):
+            return True
+        else:
+            return False
